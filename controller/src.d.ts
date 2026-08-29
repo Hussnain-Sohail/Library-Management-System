c0c3e5bcd893cf9cbd02fd0a7b4156eb10b1b260
hussnain <h63097834@gmail.com>
2026-08-30 04:54:01 +0500

    added a src.d.ts file to add token in request

---
+++ controller/src.d.ts
@@ -0,0 +1,12 @@
+import "express-serve-static-core";
+import { JwtPayload } from "jsonwebtoken";
+
+declare module "express-serve-static-core" {
+    interface Request {
+        user?: JwtPayload & {
+            userId: string;
+        };
+    }
+}
+
+export { };
