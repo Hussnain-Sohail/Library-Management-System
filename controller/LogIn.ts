9038aefdc9234c46c466d05a855ea631579bf66a
hussnain <h63097834@gmail.com>
2026-08-30 03:26:25 +0500

    same added boilerplate

---
+++ controller/LogIn.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import type { Request, Response } from "express"
+import zod from "zod"
+
+const data = zod.object({
+
+});
+async function LogIn(req: Request, res: Response): Promise<void> {
+    try {
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export default LogIn
