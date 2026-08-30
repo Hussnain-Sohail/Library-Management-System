b8a4600607f2e388704186604d93749a0df812fc
hussnain <h63097834@gmail.com>
2026-08-30 05:09:23 +0500

    Signu is complete will change if required so far done

--- controller/SignUp.ts
+++ controller/SignUp.ts
@@ -1,13 +1,53 @@
 import express from "express"
 import type { Request, Response } from "express"
 import zod from "zod"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import User from "../model/UserSchema.ts"
 
 const data = zod.object({
-
+    userName: zod.string(),
+    userAge: zod.number().min(18),
+    userPassword: zod.string(),
+    userTier: zod.string(),
 });
+
 async function SignUp(req: Request, res: Response): Promise<void> {
     try {
+        const valid_data = data.safeParse(req.body);
+        if (!valid_data.success) {
+            res.status(400).json({ message: valid_data.error.issues[0]?.message ?? "Invalid Input" });
+            return;
+        }
 
+        const hashedPassword = await bcrypt.hash(valid_data.data.userPassword, 10);
+
+        const newUser = new User({
+            userName: valid_data.data.userName,
+            userAge: valid_data.data.userAge,
+            userPassword: hashedPassword,
+            userTier: valid_data.data.userTier,
+        });
+
+        await newUser.save();
+
+        const accessToken = jwt.sign(
+            { userID: newUser._id },
+            process.env.ACCESS_TOKEN_SECRET!,
+            { expiresIn: '60s' },
+        );
+
+        const refreshToken = jwt.sign(
+            { userID: newUser._id },
+            process.env.REFRESH_TOKEN_SECRET!,
+            { expiresIn: '5m' },
+        );
+
+        res.cookie("resfreshToken", refreshToken, { httpOnly: true, maxAge: 5 * 60 });
+
+        res.status(200).json({ message: "Account created successfully", accessToken: accessToken });
     }
     catch (error) {
         console.error(error);
