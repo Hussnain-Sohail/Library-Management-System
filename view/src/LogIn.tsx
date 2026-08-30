bcf4c0107836ce3f6f38cd6a178a2c217751baea
hussnain <h63097834@gmail.com>
2026-08-30 09:47:07 +0500

    added signup and login still a few things left

---
+++ view/src/LogIn.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function LogIn() {
+
+    const [name, setUserName] = useState<string>("");
+    const [password, setUserPassword] = useState<string>("");
+
+    const [responseMessage, setResponseMessage] = useState<string>("");
+
+    const getValueString = (setter: React.Dispatch<React.SetStateAction<string>>) => {
+        return (event: React.ChangeEvent<HTMLInputElement>) => {
+            //React.Dispatch<React.SetStateAction<string>>
+            setter(event.target.value);
+        }
+    }
+
+    const submit = async (event: React.FormEvent<HTMLFormElement>) => {
+        try {
+            event.preventDefault();
+            const request = await fetch("http://localhost:3500/user/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name, password }),
+            });
+
+            if (!request.ok)
+                setResponseMessage("Could not send requeest");
+
+            const response = await request.json();
+            setResponseMessage(response.message);
+
+        }
+        catch (error) {
+            console.error(error);
+            setResponseMessage("Could not send requeest");
+        }
+    }
+
+    return (
+        <form onSubmit={submit}>
+
+            <label>Enter Name</label><br />
+            <input type="text" onChange={getValueString(setUserName)} required /><br />
+
+            <label>Enter Password</label><br />
+            <input type="password" onChange={getValueString(setUserPassword)} required /><br />
+
+            <button>Submit Request</button>
+
+            {responseMessage && <p>{responseMessage}</p>}
+
+        </form>
+    )
+}
+
+export default LogIn;
